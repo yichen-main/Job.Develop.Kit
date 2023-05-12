638e4b97a833c3ffcad44a2262eb2165d00e6277
elihu <yichen.major@gmail.com>
2023-05-12 18:00:23 +0800

    commit all

--- Clients/Modbus.Example/Program.cs
+++ Clients/Modbus.Example/Program.cs
@@ -1,13 +1,7 @@
 ﻿try
 {
-    using ModbusRtuClient client = new()
-    {
-        BaudRate = 19200,
-        Parity = Parity.None,
-        StopBits = StopBits.One
-    };
-    client.Connect("COM3");
-    client.GetDemo();
+
+     FluentModbusTool.GetDemo();
 
     Console.WriteLine("Tests finished. Press any key to continue.");
     Console.ReadLine();
---
+++ Clients/Modbus.Example/Tools/FluentModbusTool.cs
@@ -0,0 +1,17 @@
+﻿namespace Modbus.Example.Tools;
+internal static class FluentModbusTool
+{
+    internal static void GetDemo()
+    {
+        var slaveId = 0x01; //從站編號
+        var startingAddress = 0x123E; //起始位置 0x1233
+        var count = 2; //暫存器數量
+
+        //04h Read Input Registers
+        using ModbusRtuClient client = new() { BaudRate = 19200, Parity = Parity.None, StopBits = StopBits.One };
+        {
+            client.Connect("COM3");
+            var datas = client.ReadInputRegisters<uint>(slaveId, startingAddress, count);
+        }
+    }
+}
